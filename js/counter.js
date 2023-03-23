4c2fc90024c1dcb7fbfaa0f80487d69c4f904694
Hertzog9 <r.build9@gmail.com>
2023-03-23 13:23:06 +0200

    js

---
+++ js/counter.js
@@ -0,0 +1,12 @@
+
+const btnMinus = document.querySelector('[data-action="minus"]');
+const btnPlus = document.querySelector('[data-action="plus"]');
+
+btnMinus.addEventListener('clik', function() {
+    console.log('Minus clik');
+});
+
+btnPlus.addEventListener('clik', function() {
+    console.log('Plus clik');
+});
+
